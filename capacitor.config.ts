c327240de0451d60cded66ba14b62b943b6cdee3
HUCEN NAHIL <46198479+404nahil@users.noreply.github.com>
2024-10-21 21:32:56 +0500

    tet

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'boatomv',
-  webDir: 'out'
+  webDir: 'out',
+
+  server: {
+    url: 'http://10.100.116.6:3001',
+    cleartext: true
+  }
 };
 
 export default config;
